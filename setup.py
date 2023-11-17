d8571c6e7f74ee11a3001cd8a86e11e36907d5cc
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2023-11-17 11:53:30 +0100

    update mujoco dependency (2.0-2.2) and update metaworld dependency.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 # Required dependencies
 required = [
     "tensorflow>=2.0",
-    "mujoco-py<2.1,>=2.0",
-    "metaworld @ git+https://github.com/rlworkgroup/metaworld.git@0875192baaa91c43523708f55866d98eaf3facaf#egg=metaworld",
+    "mujoco-py<2.2,>=2.0",
+    "metaworld @ git+https://github.com/Farama-Foundation/Metaworld.git@lastReleaseBeforeFarama#egg=metaworld",
     "pandas",
     "matplotlib",
     "seaborn",
